46233c1948d47b64ee9bbe9ee9fc4a631b558af6
akhil <ambatiakhil1997@outlook.com>
2022-02-24 17:04:59 +0530

    update17.0

--- alphacoders/settings.py
+++ alphacoders/settings.py
@@ -27,7 +27,7 @@ with open(os.path.join(BASE_DIR,'SECRET_KEY.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['128.199.28.100','www.alphacoders.in','alphacoders.in']
  #
